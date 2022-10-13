afd73db63798623edd0944eaadf7ebc9050398bd
mildosky <ibrahimmusah205@gmail.com>
2022-10-13 10:14:13 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,12 @@
 #include<stdio.h>
 
-
-
 /**
-
- *main - Printing
-
+ * main - Printing in C
  *
-
- *Return: Always 0 (Successful)
-
+ * Return: Always 0 (Successful)
  */
-
 int main(void)
-  
 {
-  
   puts("\"Programming is like building a multilingual puzzle");
-  
   return (0);
-  
 }
