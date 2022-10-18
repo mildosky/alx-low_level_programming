ab30021bb353e255899608369b6dc67e1f6fdf3c
mildosky <ibrahimmusah205@gmail.com>
2022-10-18 14:02:05 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,23 @@
  * File: 0-putchar.c
  * Auth: Musah Ibrahim
  */
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "alx.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
 int main(void)
 {
-  char *sh = "Alx";
-  while (*sh)
-    {
-      _putchar(*sh);
-      sh++;
-    }
-  _putchar('\n');
-  return (0);
+	char _putchar[] = "Alx";
+	int i = 0;
+
+	while (_putchar[i] != '\0')
+	{
+		_putchar(_putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
