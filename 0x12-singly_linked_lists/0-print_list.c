785026cbd62367315782c319958300006a5272de
mildosky <ibrahimmusah205@gmail.com>
2022-11-24 15:10:25 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,11 @@
+#include "list.h"
 #include <stdio.h>
 #include "lists.h"
 
 /**
+ * print_list - prints all the elements of a list
+ * @h: header
+ * Return: count being i
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
@@ -9,8 +13,11 @@
  */
 size_t print_list(const list_t *h)
 {
+int i;
 	size_t n = 0;
 
+if (h == NULL)
+return (0);
 	while (h)
 	{
 		if (!h->str)
@@ -21,5 +28,26 @@ size_t print_list(const list_t *h)
 		n++;
 	}
 
+for (i = 0; h; i++)
+{
+if (h->str != NULL)
+{
+printf("[%u] ", h->len);
+}
+else
+{
+printf("[%u] ", 0);
+}
+if (h->str != NULL)
+{
+printf("%s\n", h->str);
+}
+else
+{
+printf("%s\n", "(nil)");
+}
+h = h->next;
+}
+return (i);
 	return (n);
 }
