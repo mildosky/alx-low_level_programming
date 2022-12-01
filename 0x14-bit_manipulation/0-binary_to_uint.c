71bcd02b298a7a894b6e6ab495051b5d0528d4b9
mildosky <ibrahimmusah205@gmail.com>
2022-12-01 15:57:56 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,53 @@
-#include "holberton.h"
+/**
+ * _pow - Computes x raised to power of y.
+ * @x: The mantisse.
+ * @y: The exposant.
+ * Return: x ^ y.
+ */
+int _pow(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	return (x * _pow(x, y - 1));
+}
+
 
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ * _strlen - Returns the length of a string.
+ * @s: The string whose the length should be returned.
+ * Return: The length of s.
+ */
+int _strlen(const char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen(s + 1));
+}
+
+
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: the binary to convert.
+ * Return: The unsigned int representation of the binary number.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int res = 0;
+	int i = 0, n;
 
+	n = _strlen(b);
 	if (!b)
 		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+	for (i = 0; i < n; i++)
+		if (b[i] == '1')
+			res += _pow(2, n - 1 - i);
+		else if (b[i] != '0')
 			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-	}
-
-	return (ui);
+	return (res);
 }
