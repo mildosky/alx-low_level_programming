91b8775d1cdba81fa3335c79748832b24cd1256e
mildosky <ibrahimmusah205@gmail.com>
2022-12-01 16:15:16 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,13 +1,17 @@
 #include "main.h"
-
 /**
- * get_endianness - checks the endianness
- * Return: 0 if big endian, 1 if little endian
+ * get_endianness - get the endiannes of the arch
+ * Description: get the endiannes of the arch
+ * section header: the header of this function is holberton.h
+ * Return: 1 in little indian - 0 in big indian.
  */
+
 int get_endianness(void)
 {
 	unsigned int i = 1;
-	char *c = (char *) &i;
+	char *c = (char *)&i;
 
-	return (*c);
+	if (*c)
+		return (1);
+	return (0);
 }
